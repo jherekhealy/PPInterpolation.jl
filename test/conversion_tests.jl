abfc9f6fc5f7ed9bfa6c25d4ba1e7127a6cf1e6f
Jherek Healy <jherekhealy@protonmail.com>
2023-04-18 18:38:00 +0200

    backward compat with older Julia of unit test
    Change-Id: I8bd36fb00c5dd112808ef183282ccf1a734b9dc0

--- test/conversion_tests.jl
+++ test/conversion_tests.jl
@@ -9,7 +9,7 @@ using Test, BSplines, PPInterpolation
     spl = BSplines.Spline(basis,a)
     pp = convert(PPInterpolation.PP{3,Float64,Float64}, spl)
     spl2 = convert(BSplines.Spline,pp)
-    xt = range(start=x[1]*0.75, stop=x[end]*1.25, length=101)
+    xt = range(x[1]*0.75, stop=x[end]*1.25, length=101)
     y = spl.(xt)
     y2 = spl2.(xt)
     @test isapprox(y,y2,atol=1e-15)        
